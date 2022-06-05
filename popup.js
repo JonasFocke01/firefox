677de82a0ee2912bb13f1cd96282d9aa3023a31e
jonas <jonas-focke@mailbox.org>
2022-06-05 15:50:35 +0200

    fixed manifest errors

--- popup.js
+++ popup.js
@@ -3,7 +3,9 @@ async function loadOptions() {
   iobrokerInstanceUrl = options.iobrokerInstanceUrl
     ? options.iobrokerInstanceUrl
     : "https://www.iobroker.net/";
-  document.getElementById("scaled-frame").src = iobrokerInstanceUrl;
+  // document.getElementById("scaled-frame").src = iobrokerInstanceUrl;
+  document.getElementById("scaled-frame").src = 'http://192.168.2.100:8082/vis/index.html#addon';
+  
 }
 
 document.addEventListener("DOMContentLoaded", loadOptions);
