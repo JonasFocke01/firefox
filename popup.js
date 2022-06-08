e878ddbeaf58068d7e8e48004e6fdc6f67caba94
Jonas <jonas-focke@mailbox.org>
2022-06-08 17:44:41 +0200

    ran prettier

--- popup.js
+++ popup.js
@@ -2,10 +2,8 @@ async function loadOptions() {
   const options = await browser.storage.sync.get();
   iobrokerInstanceUrl = options.iobrokerInstanceUrl
     ? options.iobrokerInstanceUrl
-    : "https://www.iobroker.net/";
-  // document.getElementById("scaled-frame").src = iobrokerInstanceUrl;
-  document.getElementById("scaled-frame").src = 'http://192.168.2.100:8082/vis/index.html#addon';
-  
+    : 'https://www.iobroker.net/';
+  document.getElementById('scaled-frame').src = iobrokerInstanceUrl;
 }
 
-document.addEventListener("DOMContentLoaded", loadOptions);
+document.addEventListener('DOMContentLoaded', loadOptions);
